d14b41902e0cab8763a464e50fd8aaa353a20711
sxend <arimitsu.k+github@gmail.com>
2018-04-25 22:47:25 +0900

    commented out

--- src/main/scala/gr/UseJedis.scala
+++ src/main/scala/gr/UseJedis.scala
@@ -1,13 +1,11 @@
 package gr
 
-import java.sql.DriverManager
-
 import redis.clients.jedis.Jedis
 
 trait UseJedis {
   def useJedis: Unit = {
-    val jedis = new Jedis("localhost")
-    jedis.set("foo", "bar")
-    println(s"foo: ${jedis.get("foo")}")
+    //    val jedis = new Jedis("localhost")
+    //    jedis.set("foo", "bar")
+    //    println(s"foo: ${jedis.get("foo")}")
   }
 }
