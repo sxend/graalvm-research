f0683801b266a057dc2220bb285609a6d65c44ee
sxend <arimitsu.k+github@gmail.com>
2018-04-26 22:53:33 +0900

    refactor

--- src/main/scala/gr/UseNetty.scala
+++ src/main/scala/gr/UseNetty.scala
@@ -4,27 +4,23 @@ import java.nio.charset.Charset
 
 import io.netty.bootstrap.ServerBootstrap
 import io.netty.buffer.Unpooled
-import io.netty.channel.{ ChannelFactory, ChannelFuture, ChannelFutureListener, ChannelHandlerContext, ChannelInboundHandlerAdapter, ChannelInitializer, ServerChannel }
 import io.netty.channel.nio.NioEventLoopGroup
 import io.netty.channel.socket.SocketChannel
 import io.netty.channel.socket.nio.NioServerSocketChannel
+import io.netty.channel.{ ChannelFactory, ChannelFutureListener, ChannelInitializer, ServerChannel }
 
 trait UseNetty {
   self: UseTypesafeConfig =>
   def useNetty: Unit = {
-    val worker = new NioEventLoopGroup()
-    val bootstrap = new ServerBootstrap()
-      .group(worker)
+    new ServerBootstrap()
+      .group(new NioEventLoopGroup())
       .channelFactory(new ChannelFactory[ServerChannel] {
         override def newChannel(): ServerChannel = new NioServerSocketChannel()
       })
       .childHandler(new ChannelInitializer[SocketChannel] {
-        override def initChannel(ch: SocketChannel): Unit = {
-          ch.writeAndFlush(Unpooled.copiedBuffer("Bye\n", Charset.defaultCharset))
-            .addListener(ChannelFutureListener.CLOSE)
-        }
+        override def initChannel(ch: SocketChannel): Unit =
+          ch.writeAndFlush(Unpooled.EMPTY_BUFFER).addListener(ChannelFutureListener.CLOSE)
       })
-    val server = bootstrap.bind(config.getInt("gr.server.port")).sync()
-    worker.shutdownGracefully()
+    //      .bind(config.getInt("gr.server.port"))
   }
 }
