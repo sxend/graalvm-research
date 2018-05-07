6a16d71a524b79989ce22930d33f83568985a4bc
sxend <arimitsu.k+github@gmail.com>
2018-05-07 15:09:15 +0900

    add http

--- src/main/scala/gr/UseNetty.scala
+++ src/main/scala/gr/UseNetty.scala
@@ -7,7 +7,10 @@ import io.netty.buffer.Unpooled
 import io.netty.channel.nio.NioEventLoopGroup
 import io.netty.channel.socket.SocketChannel
 import io.netty.channel.socket.nio.NioServerSocketChannel
-import io.netty.channel.{ ChannelFactory, ChannelFutureListener, ChannelInitializer, ServerChannel }
+import io.netty.channel._
+import io.netty.handler.codec.http._
+import HttpResponseStatus._
+import HttpVersion._
 
 trait UseNetty {
   self: UseTypesafeConfig =>
@@ -19,8 +22,28 @@ trait UseNetty {
       })
       .childHandler(new ChannelInitializer[SocketChannel] {
         override def initChannel(ch: SocketChannel): Unit =
-          ch.writeAndFlush(Unpooled.EMPTY_BUFFER).addListener(ChannelFutureListener.CLOSE)
+          ch.pipeline().addLast(new HttpServerCodec()).addLast(new MyHandler)
       })
-    //      .bind(config.getInt("gr.server.port"))
+//      .bind(config.getInt("gr.server.port"))
+  }
+}
+
+class MyHandler extends ChannelInboundHandlerAdapter {
+  override def channelRead(ctx: ChannelHandlerContext, msg: scala.Any): Unit = {
+    msg match {
+      case req: HttpRequest =>
+        if (HttpUtil.is100ContinueExpected(req)) ctx.write(new DefaultFullHttpResponse(HTTP_1_1, CONTINUE))
+        val response = new DefaultFullHttpResponse(HTTP_1_1, OK, Unpooled.copiedBuffer("hi", Charset.defaultCharset()))
+        response.headers.set(HttpHeaderNames.CONTENT_TYPE, "text/plain")
+        ctx.writeAndFlush(response).addListener(ChannelFutureListener.CLOSE)
+      case _ =>
+    }
+  }
+  override def channelReadComplete(ctx: ChannelHandlerContext): Unit = {
+    ctx.flush()
+  }
+  override def exceptionCaught(ctx: ChannelHandlerContext, cause: Throwable): Unit = {
+    cause.printStackTrace()
+    ctx.close()
   }
 }
